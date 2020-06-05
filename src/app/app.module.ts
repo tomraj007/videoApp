a28e57bc9985d6c16787e388b96976d09fd30d78
unknown <tomraj007@gmail.com>
2020-06-05 14:43:30 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularAgoraRtcModule, AgoraConfig } from 'angular-agora-rtc'; 
 
+const agoraConfig: AgoraConfig = {
+  AppID: '0ef849b57a0e44279e825300f8d69b83',
+};
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularAgoraRtcModule.forRoot(agoraConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
